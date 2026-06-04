e16b4d2d35f6a1ef33f755128be93146806aaf4f
ThienPham <thienphamuser@gmail.com>
2026-06-04 14:49:18 +0700

    chore: remove apps/extension package and dependencies

--- apps/extension/src/entrypoints/sidepanel/index.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from "react-dom/client";
-import { DictionaryView } from "@/components/DictionaryView";
-
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<DictionaryView />);
